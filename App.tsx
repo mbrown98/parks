5054ebb15b6ae9cb69a0ae34f6e127d60d177bd1
Matthew Brown <mbrown@geomarvel.com>
2022-01-25 19:37:52 -0500

    temp splash added

--- App.tsx
+++ App.tsx
@@ -1,16 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppLoading from "expo-app-loading";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [ready, setReady] = useState<boolean>(false);
 
   if (!isLoadingComplete) {
-    return null;
+    return (
+      <AppLoading
+        startAsync={async () => {
+          setTimeout(() => {}, 3000);
+        }}
+        onFinish={() => {}}
+        onError={(err) => console.log(err)}
+      />
+    );
   } else {
     return (
       <SafeAreaProvider>
