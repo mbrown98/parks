3b4929d6623acc8eca5f1cb62ef3eb3697414a94
Matthew Brown <mbrown@geomarvel.com>
2022-01-25 20:12:29 -0500

    dev work with bgimg component

--- screens/Home/Home.styles.ts
+++ screens/Home/Home.styles.ts
@@ -3,7 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   pageContain: {
     flex: 1,
-    backgroundColor: "green",
+  },
+  bgImg: {
+    flex: 1,
+    opacity: 0.6,
   },
 });
 
--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import styles from "./Home.styles";
 
 export default function Home() {
   return (
     <View style={styles.pageContain}>
-      <Text>HI</Text>
+      <ImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1551336841-32a98a5917eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1508&q=80",
+        }}
+        resizeMode="cover"
+        style={styles.bgImg}
+      ></ImageBackground>
     </View>
   );
 }
